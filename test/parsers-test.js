a2e364c36a65008c6d960c172a297f3f96b42846
Jeff S <jeff.shep777@gmail.com>
2023-04-27 13:28:47 -0600

    Add skeleton structure and imports for jsonParser and related tests

---
+++ test/parsers-test.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import parseJsonData from '../src/parsers/jsonParser.js'
+
+
+describe('parseJsonData', function() {
+
+  it('should be a function', function() {
+    expect(parseJsonData).to.be.a('function')
+  })
+
+
+})
