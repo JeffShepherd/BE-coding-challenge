d4f3f44069ba9d1d2a0d7e4dc2c993b16cc2e024
Jeff S <jeff.shep777@gmail.com>
2023-04-27 14:31:18 -0600

    Update parseJsonData function so it passes all tests

--- src/parsers/jsonParser.js
+++ src/parsers/jsonParser.js
@@ -1,7 +1,18 @@
 import fs from 'fs'
 
-const parseJsonData = () => {
+const parseJsonData = (path) => {
+  return new Promise((resolve, reject) => {
 
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        const jsonData = JSON.parse(data)
+        resolve(jsonData)
+      }
+    })
+
+  })
 }
 
 export default parseJsonData
