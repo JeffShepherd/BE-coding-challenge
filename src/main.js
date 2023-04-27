35151e4f97de7bf87a6c2a31c6028499f7ada7f9
Jeff S <jeff.shep777@gmail.com>
2023-04-27 16:33:01 -0600

    Add variables to represent data returned from parsing functions

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import parseJsonData from './parsers/jsonParser.js'
 import parseCsvData from './parsers/csvParser.js'
 
 
-processClickCounts()
-
-function processClickCounts() {
-
+processClickCounts('./data/encodes.csv','./data/decodes.json')
+////
+export async function processClickCounts(csvFilePath, jsonFilePath) {
+  const initialClickCounts = await parseCsvData(csvFilePath)
+  const shortLinks = Object.keys(initialClickCounts)
+  const clickData = await parseJsonData(jsonFilePath)
 
 }
