5dbbfad6d7acdf9716743c1fc500a0f6903f65c4
Jeff S <jeff.shep777@gmail.com>
2023-04-27 16:56:05 -0600

    Add countValidClicks function

--- src/main.js
+++ src/main.js
@@ -8,5 +8,23 @@ export async function processClickCounts(csvFilePath, jsonFilePath) {
   const initialClickCounts = await parseCsvData(csvFilePath)
   const shortLinks = Object.keys(initialClickCounts)
   const clickData = await parseJsonData(jsonFilePath)
+  
 
+  const updatedClickCounts = countValidClicks(initialClickCounts, shortLinks, clickData)
+  
+}
+
+
+////
+export function countValidClicks(clickCounts, shortLinks, clickData) {
+  for(let i=0; i<clickData.length; i++) {
+    const domainWithHash = clickData[i].bitlink.split('//')[1]
+
+    if(!shortLinks.includes(domainWithHash)) {
+      continue
+    } else if(clickData[i].timestamp.startsWith('2021')) {
+      clickCounts[domainWithHash].count ++
+    }
+  }
+  return clickCounts
 }
